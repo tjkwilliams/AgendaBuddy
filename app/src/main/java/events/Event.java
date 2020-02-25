b74bae32e9a6981afff61509f83efcd5ba3903c2
pricejoshua <joshua.j.price@my.wheaton.edu>
2020-02-25 10:54:58 -0600

    Added Event.java and DateTime.java

--- app/src/main/java/events/Event.java
+++ app/src/main/java/events/Event.java
@@ -1,4 +1,34 @@
 package events;
 
 public class Event {
+
+    private String name;
+    private int priority;
+    private String details;
+    private DateTime startDate;
+    private DateTime endDate;
+
+    public Event(String name, DateTime startDate, DateTime endDate, int priority, String details){
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.priority = priority;
+        this.details = details;
+    }
+
+    public void changeStartTime(int year, int month, int day, int hour, int minute){
+        startDate.setYear(year);
+        startDate.setMonth(month);
+        startDate.setDay(day);;
+        startDate.setHour(hour);
+        startDate.setMinute(minute);
+    }
+
+    public void changeEndTime(int year, int month, int day, int hour, int minute){
+        endDate.setYear(year);
+        endDate.setMonth(month);
+        endDate.setDay(day);;
+        endDate.setHour(hour);
+        endDate.setMinute(minute);
+    }
 }
