3454258cfda7a66b98185bb3e2e3041658869c2b
Timothy Williams <tj.williams@my.wheaton.edu>
2020-02-25 11:32:45 -0600

    Added some action listeners to some of the buttons! First few exciting steps towards a cool app!!!!

--- app/src/main/java/com/example/myapplication/SideMenu.java
+++ app/src/main/java/com/example/myapplication/SideMenu.java
@@ -64,7 +64,7 @@ public class SideMenu extends AppCompatActivity  implements NavigationView.OnNav
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send, R.id.nav_home)
+                R.id.nav_tools, R.id.nav_share, R.id.nav_send, R.id.nav_login)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
