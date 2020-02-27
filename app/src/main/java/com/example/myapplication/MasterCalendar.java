7e6a2bbc5146775738765e8c9c1292992c7d8d18
briandrown <brianjdrown@gmail.com>
2020-02-26 21:41:36 -0600

    added calendarView instance variable and get reference to the actual calendar widget and just changed the week to start with monday instead of sunday

--- app/src/main/java/com/example/myapplication/MasterCalendar.java
+++ app/src/main/java/com/example/myapplication/MasterCalendar.java
@@ -3,12 +3,22 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
 
 public class MasterCalendar extends AppCompatActivity {
+
+    /*
+     * To store a calenderView widget
+     */
+    private CalendarView calendarView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_master_calendar);
+
+        calendarView = findViewById(R.id.calendarView); // get the calendar "object"
+
     }
 }
