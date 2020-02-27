322a654240e55578feed0ae6e5179c06344f2a00
briandrown <brianjdrown@gmail.com>
2020-02-27 17:00:54 -0600

    Used open source code for calendar --> reason: it is easier to customize. Im going to try to use this for now. If it works fine then we'll just keep using it.

--- app/src/main/java/com/example/myapplication/MasterCalendar.java
+++ app/src/main/java/com/example/myapplication/MasterCalendar.java
@@ -1,9 +1,20 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.CalendarView;
+
+import com.applandeo.materialcalendarview.EventDay;
+import com.example.myapplication.events.Event;
+
+import java.util.*;
 
 public class MasterCalendar extends AppCompatActivity {
 
@@ -18,7 +29,25 @@ public class MasterCalendar extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_master_calendar);
 
-        calendarView = findViewById(R.id.calendarView); // get the calendar "object"
+        List<Event> events = new ArrayList<>();
+
+        Calendar calendar = Calendar.getInstance();
+        // use local Event class made by @Joshua
+
+        //events.add(new EventDay(calendar, R.drawable.ic_launcher_foreground, Color.parseColor("#228B22"))); from internet
+
+        calendarView = findViewById(R.id.calendarView); // get the calendar "object
+        //calendarView.setEvents(events); from internet
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                // do something when we select different dates at the calendar
+            }
+        });
+
+        
+
 
     }
 }
