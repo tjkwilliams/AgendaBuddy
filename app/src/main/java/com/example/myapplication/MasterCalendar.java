570ba3ac1eca1c7aa6eb951cdd126351a7c0345e
briandrown <brianjdrown@gmail.com>
2020-03-03 11:02:11 -0600

    Added textField to show selected date from calendar Added button to link to fragment that creates new event

--- app/src/main/java/com/example/myapplication/MasterCalendar.java
+++ app/src/main/java/com/example/myapplication/MasterCalendar.java
@@ -10,6 +10,7 @@ import android.graphics.ColorFilter;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.CalendarView;
+import android.widget.TextView;
 
 import com.applandeo.materialcalendarview.EventDay;
 import com.example.myapplication.events.Event;
@@ -22,7 +23,9 @@ public class MasterCalendar extends AppCompatActivity {
      * To store a calenderView widget
      */
     private CalendarView calendarView;
-    List<Event> events;
+    private List<Event> events;
+    private TextView myDate;
+
 
 
     @Override
@@ -30,27 +33,34 @@ public class MasterCalendar extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_master_calendar);
 
-        events = new ArrayList<Event>();
+        events = new ArrayList<Event>(11);
 
         Calendar calendar = Calendar.getInstance();
-        // use local Event class made by @Joshua to create new event
+        calendar.set(2020,01,01);
+
 
         //events.add(new EventDay(calendar, R.drawable.ic_launcher_foreground, Color.parseColor("#228B22"))); from internet
 
         calendarView = findViewById(R.id.calendarView); // get the calendar "object
-        //calendarView.setEvents(events); from internet
+        //calendarView.setEvents(events); from internet;
+
+        myDate = findViewById(R.id.myDate);
+
 
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
                 // do something when we select different dates at the calendar
+                String date = month + "/" + dayOfMonth + "/" + year;
+                myDate.setText(date);
+
             }
         });
 
     }
 
 
-    protected boolean createEvent(Event e){
+    public boolean createEvent(Event e){
         return events.add(e);
     }
 }
