d2ebec77c2ba6bc88579b985ccbbbc3c00e24f7b
Timothy Williams <tj.williams@my.wheaton.edu>
2020-03-03 14:07:25 -0600

    Login Completed
    - Efficiency & functionality for creating & storing accounts improved.
    - Login functionality fixed. Cases covered:
        - Username or password entered is empty.
        - Account with entered username doesn't exist.
        - Duplicate account names.
        - Password checking.
    - Can now log in by pressing enter.
    - Incorrect login attempts now have cool animation.
    - Fixed all other login bugs.
    - Documentation very clear.

--- app/src/main/java/com/example/myapplication/page/LoginPage.java
+++ app/src/main/java/com/example/myapplication/page/LoginPage.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -11,124 +13,168 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.myapplication.R;
+import com.example.myapplication.ui.account.Account;
+import com.example.myapplication.ui.account.AccountMaster;
 
 public class LoginPage extends AppCompatActivity implements View.OnClickListener{
     //create instance variables
-    private EditText name;
-    private EditText password;
-    private Button login;
-    private passwordDatabase pwd;
-    private Button create;
+    private EditText username, password;
+    private Button login, create;
     private TextView info;
+    private ImageView buddyIcon;
+    private Animation shake;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        AccountMaster.initialize();
+
         //associate instance variables with the objects on the login page
-        name = (EditText)findViewById(R.id.userTextField);
+        username = (EditText)findViewById(R.id.userTextField);
         password = (EditText)findViewById(R.id.pswdTextField);
         login = (Button)findViewById(R.id.loginButton);
         create = (Button)findViewById(R.id.createButton);
         info = (TextView)findViewById(R.id.tvinfo);
-
-        ImageView loginImage = findViewById(R.id.buddyIcon);
-        loginImage.setOnClickListener(this);
+        buddyIcon = (ImageView)findViewById(R.id.buddyIcon);
 
         login.setOnClickListener(this);
         create.setOnClickListener(this);
-        name.setOnClickListener(this);
+        username.setOnClickListener(this);
         password.setOnClickListener(this);
         info.setOnClickListener(this);
-        //listener for the buttons
-        /*
-        login.setOnClickListener(new View.OnClickListener(){
+        buddyIcon.setOnClickListener(this);
 
-            @Override
-            public void onClick(View v) {
-                validate(name.getText().toString(), password.getText().toString());
-            }
-        });
-        create.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-                createAccount(name.getText().toString(), password.getText().toString());
-            }
-        });
-        */
-
+        shake = AnimationUtils.loadAnimation(this, R.anim.shake);
     }
 
-    /*
-     *   a method to validate login credentials and, if valid, transition to next page
+    /**
+     * Creates an account from the login page.
+     *
+     * @param user Username associated with the account.
+     * @param pass Password associated with the account.
      */
-    private boolean validate(String userName, String userPassword){
-        return pwd.validate(userName, userPassword);
-        /*
-        if(pwd.validate(userName, userPassword)==true){
-            Intent intent = new Intent(this, MainPage.class);
-            startActivity(intent);
-        }else{
-            info.setText("incorrect login credentials");
-            //print to screen "incorrect login credentials"
+    public void createAccount(String user, String pass){
+
+        Account a = AccountMaster.getAccount(user);
+
+        if(a != null){
+            Toast.makeText(this, "Account already exist!", Toast.LENGTH_SHORT).show();
+            return;
         }
 
-        if((userName.equals("admin")) && (userPassword.equals("1234"))){
-            //correct login
-            Intent intent = new Intent(this, MainPage.class);
-            startActivity(intent);
-        }
-        */
+        AccountMaster.createAccount(user,pass);
+
+        if(AccountMaster.hasAccount(user))
+            Toast.makeText(this, "Account created!", Toast.LENGTH_SHORT).show();
     }
 
-    /*
-     * a method to add account credentials to the login map. Simulates what will happen when we develop a
-     * database and server to store accounts
+    /**
+     * Every time an object from the login conbtent is clicked, depending
+     * on which was clicked, one of the following cases will execute.
+     *
+     * @param v The object clicked.
      */
-    private void createAccount(String userName, String userPassword){
-        pwd.create(userName, userPassword);
-        info.setText("now press LOGIN");
-        Toast.makeText(this, "DFGJNDGHSDFGHHDFGJHFGDFHDFHFSDHHSDFRFGDSRFGDHGHFDSHDFGDFHGFDGDSHFHDSFGASFDGHSDFHDFHGDFHGSDGFDSFHGHDFJHDFHGHSFDHDGDFHDGHDFHDGHDHSDFHDGHDFHGDFGFDGJHGFDHFDGHFGJFHFDJGFFDJFFDHFDGHYDRFHDTGFDHGFD", Toast.LENGTH_SHORT).show();
-    }
-
     @Override
     public void onClick(View v) {
-        switch(v.getId()){
-            case R.id.loginButton:
-                //Toast.makeText(this, "touched login", Toast.LENGTH_SHORT).show();
 
-                if(validate(name.getText().toString(), password.getText().toString())){
+        // Gets the text from the username and password textfields.
+        String u = username.getText().toString(), p = password.getText().toString();
+        boolean loginSuccessful;
+
+        switch(v.getId()) {
+
+            case R.id.loginButton:
+                loginSuccessful = AccountMaster.login(u, p);
+
+                if (loginSuccessful) {
 
                     Intent intent = new Intent(this, MainPage.class);
                     startActivity(intent);
-                }else{
-                    Toast.makeText(this, "incorrect credentials", Toast.LENGTH_SHORT).show();
 
+                } else {
+
+                    Account acc = AccountMaster.getAccount(u);
+
+                    if (acc == null)
+
+                        Toast.makeText(this, "Account doesn't exist", Toast.LENGTH_SHORT).show();
+
+                    else {
+
+                        username.startAnimation(shake);
+                        password.startAnimation(shake);
+
+                    }
                 }
 
                 break;
-            case R.id.createButton:
-                Toast.makeText(this, "touched create", Toast.LENGTH_SHORT).show();
 
-                createAccount(name.getText().toString(), password.getText().toString());
+            case R.id.createButton:
+
+                if (!AccountMaster.hasAccount(u))
+                    createAccount(u, p);
+                else
+                    Toast.makeText(this, "Sorry. That username is taken!!", Toast.LENGTH_SHORT).show();
+
                 break;
+
             case R.id.buddyIcon:
                 Toast.makeText(this, "Welcome to Agenda Buddy!!", Toast.LENGTH_SHORT).show();
                 break;
+
+            case R.id.pswdTextField:
+
+                if(u.equals("") || u.isEmpty()){
+                    Toast.makeText(this, "Must enter username.", Toast.LENGTH_SHORT).show();
+                    return;
+                } else if(p.equals("") || p.isEmpty()){
+                    Toast.makeText(this, "Must enter password.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                loginSuccessful = AccountMaster.login(u,p);
+
+                if(loginSuccessful){
+
+                    Toast.makeText(this, "Account exists.", Toast.LENGTH_SHORT).show();
+
+                } else {
+
+                    Account acc = AccountMaster.getAccount(u);
+
+                    if(acc == null)
+                        Toast.makeText(this, "Account doesn't exist", Toast.LENGTH_SHORT).show();
+
+                    else if(acc.password().equals(p)) {
+
+                        Intent intent = new Intent(this, MainPage.class);
+                        startActivity(intent);
+
+                    } else {
+
+                        username.startAnimation(shake);
+                        password.startAnimation(shake);
+
+                    }
+                }
+                break;
+
+
+            // If any of the following cases were clicked, nothing will happen.
+            // THERE AREN'T ANY BREAK STATEMENTS BECAUSE: Since not putting a break statement
+            // just executes the following case, then just have them do nothing.
+            case R.id.userTextField:
+
+            case R.id.userLabel:
+
+            case R.id.pswdLabel:
+
+                break;
+
             default:
-                Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Listener not set up yet for this button.", Toast.LENGTH_SHORT).show();
 
         }
-
     }
-        /*
-        Intent intent = new Intent(MainActivity.this, thirdActivity.class);
-        name = (EditText)findViewById(R.id.enterUsername);
-        password = (EditText)findViewById(R.id.enterPassword);
-
-        pwd.create(name.getText().toString(), password.getText().toString());
-*/
-
-
 }
--- app/src/main/java/com/example/myapplication/ui/account/AccountMaster.java
+++ app/src/main/java/com/example/myapplication/ui/account/AccountMaster.java
@@ -9,18 +9,43 @@ import java.util.HashMap;
  */
 public class AccountMaster {
 
-
+    /**
+     * The base of accounts.
+     */
     public static ArrayList<Account> accountMap;
 
-
+    /**
+     * Initializes the instance of the mock database of accounts.
+     */
     public static void initialize(){
         if(accountMap == null){
             accountMap = new ArrayList<>();
             accountMap.add(new Account("admin","admin"));
         }
-
     }
 
+        /**
+     * The details associated with an account.
+     *
+     * @param a The account to get the details from.
+     * @return The details of the account.
+     */
+    public String accountDetails(Account a){
+        if(a == null) return "DNE";
+
+        String ret = "";
+        ret += "Name: " + a.username() + "\n";
+        ret += "Password: " + a.password();
+
+        return ret;
+    }
+
+    /**
+     * The logging in of an account.
+     *
+     * @param a The account to login in.
+     * @return Whether or not the account can login.
+     */
     public static boolean login(Account a){
         if(a == null) return false;
 
@@ -31,6 +56,13 @@ public class AccountMaster {
         return false;
     }
 
+    /**
+     * The logging in of an account.
+     *
+     * @param username The username of the user's account.
+     * @param password The password of the user's account.
+     * @return Whether or not the account can login.
+     */
     public static boolean login(String username, String password){
 
         Account acc = getAccount(username);
@@ -42,18 +74,69 @@ public class AccountMaster {
         return false;
     }
 
+    /**
+     * Fetches all of the accounts.
+     *
+     * @return The list of accounts currently in the database.
+     */
+    public ArrayList<Account> accounts(){
+        return accountMap;
+    }
+
+    /**
+     * Fetches the names of all of the accounts' names.
+     *
+     * @return The list of accounts currently in the database.
+     */
+    public String accountNames(){
+        if(accountMap == null) return "NULL DATABASE";
+        if(accountMap.isEmpty()) return "EMPTY";
+
+        String ret = "";
+
+        for(Account account: accountMap){
+            ret += accountDetails(account);
+        }
+
+        return ret;
+    }
+
+    /**
+     * Creates an account in the system.
+     *
+     * @param username The username of the user's account.
+     * @param password The password of the user's account.
+     */
     public static void createAccount(String username, String password){
         accountMap.add(new Account(username, password));
     }
 
+    /**
+     * Sees if the account exists in the system.
+     *
+     * @param name The name of the account to look for.
+     * @return If the account is registered.
+     */
     public static boolean hasAccount(String name){
         return getAccount(name) != null;
     }
 
+    /**
+     * Sees if the account exists in the system.
+     *
+     * @param n The account to look for.
+     * @return If the account is registered.
+     */
     public static boolean hasAccount(Account n){
         return accountMap.contains(n);
     }
 
+    /**
+     * Gets the account associated with this name from the system.
+     *
+     * @param name Name of the account being searched for.
+     * @return The account associated with this account.
+     */
     public static Account getAccount(String name){
         for(Account acc : accountMap)
             if(acc.username().equals(name))
