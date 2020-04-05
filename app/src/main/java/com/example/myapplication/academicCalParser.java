fcf9a82f6e5edbdb74a0f68894c18c1166a3cbb8
Ally Lyle <ally.lyle@my.wheaton.edu>
2020-04-05 15:46:28 -0400

    "first draft" parsing academic calendar

---
+++ app/src/main/java/com/example/myapplication/academicCalParser.java
@@ -0,0 +1,33 @@
+package com.example.myapplication;
+
+import java.io.IOException;
+import java.util.LinkedList;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class academicCalParser {
+
+    public static void main(String args[]) throws IOException {
+        Document doc;
+        doc = Jsoup.connect("https://25livepub.collegenet.com/calendars/event-collections-general_calendar_wp.rss").get();
+        System.out.println(doc.title());
+
+        String day, month, year;
+        Elements items = doc.getElementsByTag("item");
+        for(Element item : items) {
+            Elements t = item.getElementsByTag("title");
+            System.out.println(t.text());
+            Elements d = item.getElementsByTag("category");
+            year = d.text().substring(0,4);
+            month = d.text().substring(5,7);
+            day = d.text().substring(8, 10);
+
+            Event n = new Event();
+            //Event n = wp.new Event(t.text(), getYear(d.text()), getMonth(d.text()), getDay(d.text()) );
+            //eventList.add(n);
+        }
+
+}
