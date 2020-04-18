1bfdb60887a070434f12f0853bdbd18893f4c07a
pricejoshua <joshua.j.price@my.wheaton.edu>
2020-04-18 16:00:20 -0700

    Added a connection to database

---
+++ app/src/main/java/com/example/myapplication/connect/SendToCommunityDB.java
@@ -0,0 +1,66 @@
+package com.example.myapplication.connect;
+
+
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+
+
+
+public class SendToCommunityDB {
+
+    public void send() {
+
+        HashMap<String, String> params = new HashMap<>();
+
+        params.put("title", "test");
+        params.put("location", "here");
+        params.put("startTime", "11:34:45");
+        params.put("endTime", "12:36:27");
+        params.put("eventDate", "2020-05-06");
+
+
+        StringBuilder sbParams = new StringBuilder();
+        int i = 0;
+        for (String key : params.keySet()) {
+            try {
+                if (i != 0) {
+                    sbParams.append("&");
+                }
+                sbParams.append(key).append("=")
+                        .append(URLEncoder.encode(params.get(key), "UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            i++;
+        }
+
+        try {
+            String url = "http://18.233.165.117/test.php";
+            URL urlObj = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection) urlObj.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Accept-Charset", "UTF-8");
+
+            conn.setReadTimeout(10000);
+            conn.setConnectTimeout(15000);
+
+            conn.connect();
+
+            String paramsString = sbParams.toString();
+
+            DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+            wr.writeBytes(paramsString);
+            wr.flush();
+            wr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
