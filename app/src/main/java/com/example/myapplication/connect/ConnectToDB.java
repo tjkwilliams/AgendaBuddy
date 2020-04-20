a31f96372c8fdfc6f1ee073ef037a6d9de303bff
pricejoshua <joshua.j.price@my.wheaton.edu>
2020-04-19 17:06:35 -0700

    Continued working on the database connection

--- app/src/main/java/com/example/myapplication/connect/ConnectToDB.java
+++ app/src/main/java/com/example/myapplication/connect/ConnectToDB.java
@@ -3,9 +3,9 @@ package com.example.myapplication.connect;
 import java.sql.*;
 
 public class ConnectToDB {
-    public static void addToDB () {
+    public static Connection getConnection() {
 
-        Connection conn;
+        Connection conn = null;
 
         try {
             // Step 1: "Load" the JDBC driver
@@ -15,18 +15,13 @@ public class ConnectToDB {
             String url = "jdbc:mariadb://18.233.165.117/test";
             conn = DriverManager.getConnection(url, "root", "my-new-password");
 
-            // create a Statement from the connection
-            Statement statement = conn.createStatement();
-
-            // insert the data
-            statement.executeUpdate("INSERT INTO testTable " + "VALUES ('josh', 2001, 49)");
-
         } catch (Exception e) {
             System.err.println("D'oh! Got an exception!");
             System.err.println(e.getMessage());
             e.printStackTrace();
         }
-
+        
+        return conn;
 
     }
 }
--- app/src/main/java/com/example/myapplication/connect/SendToCommunityDB.java
+++ app/src/main/java/com/example/myapplication/connect/SendToCommunityDB.java
@@ -2,65 +2,28 @@ package com.example.myapplication.connect;
 
 
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.util.HashMap;
-
+import java.sql.Connection;
+import java.sql.Statement;
 
 
 public class SendToCommunityDB {
 
-    public void send() {
+    public void send(String title, String startTime, String endTime, String date) {
 
-        HashMap<String, String> params = new HashMap<>();
-
-        params.put("title", "test");
-        params.put("location", "here");
-        params.put("startTime", "11:34:45");
-        params.put("endTime", "12:36:27");
-        params.put("eventDate", "2020-05-06");
-
-
-        StringBuilder sbParams = new StringBuilder();
-        int i = 0;
-        for (String key : params.keySet()) {
-            try {
-                if (i != 0) {
-                    sbParams.append("&");
-                }
-                sbParams.append(key).append("=")
-                        .append(URLEncoder.encode(params.get(key), "UTF-8"));
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
-            i++;
-        }
+        Connection conn;
+        conn = ConnectToDB.getConnection();
 
         try {
-            String url = "http://18.233.165.117/test.php";
-            URL urlObj = new URL(url);
-            HttpURLConnection conn = (HttpURLConnection) urlObj.openConnection();
-            conn.setDoOutput(true);
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("Accept-Charset", "UTF-8");
+            // create a Statement from the connection
+            Statement statement = conn.createStatement();
 
-            conn.setReadTimeout(10000);
-            conn.setConnectTimeout(15000);
+            String query = "INSERT INTO community(title, startTime, endTime, eventDate) " +
+                    "VALUES (" + title + "," + startTime + "," + endTime + "," + startTime + ");";
+            // insert the data
+            statement.executeUpdate(query);
 
-            conn.connect();
-
-            String paramsString = sbParams.toString();
-
-            DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
-            wr.writeBytes(paramsString);
-            wr.flush();
-            wr.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
         }
     }
 }
