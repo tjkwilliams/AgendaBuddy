3e28f0a2f3d1a4e752b2037efff9da2f9de4cec1
pricejoshua <joshua.j.price@my.wheaton.edu>
2020-04-27 15:51:57 -0700

    Created Database links

---
+++ app/src/main/java/com/example/myapplication/connect/HttpDBRequest.java
@@ -0,0 +1,71 @@
+//package com.example.myapplication.connect;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpDBRequest {
+
+    public static void main(String args[]) {
+        getEvent(5);
+    }
+
+    public static void getEvent(int eid){
+        try {
+            URL url = new URL("http://18.233.165.117/agenda-buddy/get_event_details.php");
+            String postData = "eid=" + eid;
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestProperty("Content-Length", Integer.toString(postData.length()));
+            conn.setUseCaches(false);
+
+            try (DataOutputStream dos = new DataOutputStream(conn.getOutputStream())) {
+                dos.writeBytes(postData);
+            }
+
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    System.out.println(line);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void addEvent(){
+        try {
+            URL url = new URL("http://18.233.165.117/agenda-buddy/create_event.php");
+            String postData = "title=test&location=test2&start_time=13:45&end_time=14:45&&year=2020&month=05&day=23";
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestProperty("Content-Length", Integer.toString(postData.length()));
+            conn.setUseCaches(false);
+
+            try (DataOutputStream dos = new DataOutputStream(conn.getOutputStream())) {
+                dos.writeBytes(postData);
+            }
+
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    System.out.println(line);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
