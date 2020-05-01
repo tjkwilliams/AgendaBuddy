26a7d6a7b554435bdc36d7fb59316e59a40a0ab3
briandrown <brianjdrown@gmail.com>
2020-05-01 10:36:24 -0500

    change color schemes

--- app/src/main/java/com/example/myapplication/customcalendar/MyGridAdapter.java
+++ app/src/main/java/com/example/myapplication/customcalendar/MyGridAdapter.java
@@ -60,7 +60,7 @@ public class MyGridAdapter extends ArrayAdapter {
         }
 
         if(displayMonth == currentMonth && displayYear == currentYear) {
-            view.setBackgroundColor(getContext().getResources().getColor(R.color.greyGreen));
+            view.setBackgroundColor(getContext().getResources().getColor(R.color.greyOrange));
         } else {
             view.setBackgroundColor(Color.parseColor("#CCCCCC"));
         }
